da8bd0ab58bc69f9f24e05bf4451aba05d72c427
Mahmoud Eid <mahmoudeid1840@gmail.com>
2022-10-30 14:13:36 +0200

    slide

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const selector=(ele)=>document.querySelector(ele);
+const selectorAll=(all)=>document.querySelectorAll(all);
+
 selector('.mobile-menu').addEventListener('click',()=>{
     selector('header').classList.toggle('active');
     selector('.nav-list-larger').style.display="block";
@@ -8,3 +10,47 @@ selector('.mobile-menu').addEventListener('click',()=>{
         selector('.nav-list-larger').style.opacity='0';
     }
 })
+
+
+
+const rightArrow=selector('.right');
+const leftArrow=selector('.left');
+const productsContainer=selector('.product-container');
+const products=selectorAll('.product');
+const productLength=products.length;
+const lastProduct=products[productLength-1];
+const firstItem=products[0]
+// console.log(rightArrow,leftArrow,productsContainer,products,firstProduct)
+
+rightArrow.addEventListener('click',()=>{
+    products.forEach((product)=>{
+        let productwidth=product.getBoundingClientRect().width;
+        productsContainer.scrollLeft +=  productwidth ;
+        leftArrow.style.display="flex"
+       
+    })
+    if(isInViewport(lastProduct)){
+     rightArrow.style.display="none"
+    }
+})
+
+leftArrow.addEventListener('click',()=>{
+    products.forEach((product)=>{
+        let productwidth=product.getBoundingClientRect().width;
+        productsContainer.scrollLeft -=  productwidth ;
+    })
+    rightArrow.style.display="flex"
+    
+})
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
